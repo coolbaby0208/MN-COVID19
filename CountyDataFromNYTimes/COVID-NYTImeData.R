2333459a98cb2690cb7b32fea99ca99d78a0ef6a
coolbaby0208 <63076072+coolbaby0208@users.noreply.github.com>
2020-04-16 15:16:56 -0500

    Add files via upload

--- CountyDataFromNYTimes/COVID-NYTImeData.R
+++ CountyDataFromNYTimes/COVID-NYTImeData.R
@@ -2,26 +2,39 @@ library(tidyverse)
 library(ggpubr)
 library(usmap)
 
+
+## function for preprocess county data from NYtimes ####
+## x = dataset from NYtime, y = state name
+preprocessCountyData = function(x, y) {
+  result = x %>% 
+    filter(state == y, cases > 0) %>%
+    arrange(desc(cases)) %>% 
+    pivot_longer(cols = c(cases,deaths)) %>% 
+    group_by(county, name) %>% 
+    mutate(diff = value -lead(value)) %>% 
+    ungroup() %>% 
+    filter(date == last(date))
+  return(result)
+}
+
+
 ## Source: https://github.com/nytimes/covid-19-data
 ## Pull data for NY-TImes github 
-counties = read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+
+## read in data ####
+counties = read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv") 
 
 states = read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+
 countyMNPopulation = read_csv("MNCountyPopulation.csv") %>% 
   mutate(county = str_remove(County," County")) %>% 
   select(-County)
 
-countyMN = counties %>% 
-  filter(state == "Minnesota", cases > 0) %>%
-  arrange(desc(cases)) %>% 
-  pivot_longer(cols = c(cases,deaths)) %>% 
-  group_by(county, name) %>% 
-  mutate(diff = value -lead(value)) %>% 
-  ungroup() %>% 
-  filter(date == last(date)) %>%
+countyMN = preprocessCountyData (counties, "Minnesota") %>%
   left_join(countyMNPopulation, by = "county") %>% 
   mutate(value = ifelse(name == "deaths", -1*value, value)) %>% 
   mutate(value10KPerCapita = value/Population*10000) 
+
 ## bars
 # p1 = ggplot(countyMN, aes(x= reorder(county, value), value, fill = name, label = value))+
 #   geom_col() +
@@ -37,19 +50,20 @@ countyMN = counties %>%
 #   coord_flip()+
 #   theme_minimal()+
 #   ggtitle(last(countyMN$date))
-  
+fontsize = 3  
 
 ## lollipop 
 p1 = ggplot(countyMN, aes(x= reorder(county, value), value,  color = name, fill = name, label = value))+
   geom_segment(aes(x = reorder(county, value), xend=reorder(county, value), y=0, yend=value), color = "gray60")+
-  geom_point(data=countyMN %>% filter(value != 0), aes(x= reorder(county, value)), size=3, shape=21, stroke=2)+
-  geom_text(data=countyMN %>% filter(value > 0), aes(x= reorder(county, value)), nudge_y = 15, color = "gray20")+
+  geom_point(data=countyMN %>% filter(value != 0), aes(x= reorder(county, value)), size=2, shape=21, stroke=2)+
+  ## case number today
+  geom_text(data=countyMN %>% filter(value > 0), aes(x= reorder(county, value)), nudge_y = 25, color = "gray20", size = fontsize)+
   ## death number today
-  geom_text(data=countyMN %>% filter(value < 0) %>% mutate(valueLabel = abs(value)), aes(x= reorder(county, value), label = valueLabel), nudge_y = .75*min(countyMN %>% filter(name == "deaths") %>% pull(value)), color = "gray20")+
+  geom_text(data=countyMN %>% filter(value < 0) %>% mutate(valueLabel = abs(value)), aes(x= reorder(county, value), label = valueLabel), nudge_y = .5*min(countyMN %>% filter(name == "deaths") %>% pull(value)), color = "gray20", size = fontsize )+
   ## increase case from previous day
-  geom_text(data=countyMN %>% filter(diff > 0, name == "cases") %>% mutate(valueLabel = paste0("(+", as.character(diff),")")), aes(x= reorder(county, value), label = valueLabel), nudge_y = .12*max(countyMN %>% filter(name == "cases") %>% pull(value)), color = "gray20")+
+  geom_text(data=countyMN %>% filter(diff > 0, name == "cases") %>% mutate(valueLabel = paste0("(+", as.character(diff),")")), aes(x= reorder(county, value), label = valueLabel), nudge_y = .1*max(countyMN %>% filter(name == "cases") %>% pull(value)), color = "gray20", size = fontsize )+
   ## increase death from previous day
-  geom_text(data=countyMN %>% filter(diff > 0, name == "deaths") %>% mutate(valueLabel = paste0("(+", as.character(diff),")")), aes(x= reorder(county, value), label = valueLabel), nudge_y = 1.65*min(countyMN %>% filter(name == "deaths") %>% pull(value)), color = "gray20")+
+  geom_text(data=countyMN %>% filter(diff > 0, name == "deaths") %>% mutate(valueLabel = paste0("(+", as.character(diff),")")), aes(x= reorder(county, value), label = valueLabel), nudge_y = 1.2*min(countyMN %>% filter(name == "deaths") %>% pull(value)), color = "gray20", size = fontsize)+
   labs(x = "", y = "Number of cases", fill = "", color = "")+
   coord_flip()+
   theme_minimal()+
@@ -70,9 +84,9 @@ p1 = ggplot(countyMN, aes(x= reorder(county, value), value,  color = name, fill 
 ## lollipop 
 p2 = ggplot(countyMN, aes(x= reorder(county, value), value10KPerCapita, color = name, fill = name, label = sprintf("%0.2f", round(value10KPerCapita, digits = 2))))+
   geom_segment(aes(x=reorder(county, value), xend=reorder(county, value), y=0, yend=value10KPerCapita), color = "gray60") +
-  geom_point(data=countyMN %>% filter(value != 0), size=3, shape=21, stroke=2)+
-  geom_text(data=countyMN %>% filter(value > 0), aes(x= reorder(county, value)), nudge_y = 1, color = "gray20")+
-  geom_text(data=countyMN %>% filter(value < 0) %>% mutate(valueLabel = abs(value10KPerCapita)), aes(x= reorder(county, value), label = sprintf("%0.2f", round(valueLabel, digits = 2))), nudge_y = -1, color = "gray20")+
+  geom_point(data=countyMN %>% filter(value != 0), size=2, shape=21, stroke=2)+
+  geom_text(data=countyMN %>% filter(value > 0), aes(x= reorder(county, value)), nudge_y = 1, color = "gray20", size = fontsize)+
+  geom_text(data=countyMN %>% filter(value < 0) %>% mutate(valueLabel = abs(value10KPerCapita)), aes(x= reorder(county, value), label = sprintf("%0.2f", round(valueLabel, digits = 2))), nudge_y = -1, color = "gray20", size = fontsize)+
   labs(x = "", y = "Number of cases per 10k capita", fill = "", color = "")+
   coord_flip()+
   theme_minimal()+
@@ -118,3 +132,20 @@ ggsave("MNcounty_COVID-19_Map.png",
 
 ## Combine both figures
 ggsave("MNcounty_COVID-19_Combined.png", gridExtra::grid.arrange(fig1, fig2, nrow = 2 , ncol = 1, heights = c(1.5,1)), width = 12, height = 14)
+
+#### Just for Ken (Utah data) ####
+countyUT = preprocessCountyData (counties, "Utah")
+p7 = plot_usmap(data = countyUT %>% filter(name == "cases"), values = "value", include = c("UT"), color = "black")+ 
+  scale_fill_continuous(low = "peachpuff", high = "red", name = "Total cases", na.value = 'white')+ 
+  #labs(title = "COVID-19 data by county", subtitle = last(countyMN$date)) +
+  theme(legend.position = "right", plot.margin = unit(c(1, 0.5, .1, 1), "cm"))
+
+p8 = plot_usmap(data = countyUT %>% 
+                  filter(name == "deaths") %>% 
+                  mutate(value = abs(value)), values = "value", include = c("UT"), color = "black")+ 
+  scale_fill_continuous(low = "snow2", high = "black", name = "Total deaths", na.value = 'white')+ 
+  #labs(title = "COVID-19 data by county", subtitle = last(countyMN$date)) +
+  theme(legend.position = "right", plot.margin = unit(c(.5, .5, 1, 1), "cm"))
+
+ggsave("UTcounty_COVID-19_Map.png", 
+       annotate_figure(ggarrange(p7, p8, nrow = 1, ncol = 2), fig.lab = paste("COVID-19 by UT county:", last(countyUT$date))), width = 10, height = 6)
