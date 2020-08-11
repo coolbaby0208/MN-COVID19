b2d54b03f06d66cd620aa974a1049fc14f125f18
coolbaby0208 <63076072+coolbaby0208@users.noreply.github.com>
2020-08-11 11:47:49 -0500

    Add files via upload

--- Misc/testGrowthRate.R
+++ Misc/testGrowthRate.R
@@ -9,7 +9,7 @@ p1 = dataWide %>%
   labs(y = "Test growth rate (%)", title = "MN COVID-19 testing", subtitle = "Date since Mar20, excluding Mar25 (5663 daily tests)",
        caption = "Formula: (current tests - past tests) / past tests", x = "")+
   theme_minimal() +
-  theme(plot.caption = element_text(size = 6))
+  theme(plot.subtitle = element_text(size = 8))
 
 p2 = dataWide %>% 
   mutate(Date = Date %>% as.Date(format = "%m/%d/%y"), 
@@ -19,9 +19,11 @@ p2 = dataWide %>%
   geom_boxplot(width = .55, outlier.colour = NA, alpha = .5, size = .25)+
   geom_jitter(height = 0, width = .25, shape = 21, stroke = 1, size = .8, color = RColorBrewer::brewer.pal(3, "Dark2")[1])+
   labs(y = "Daily tests", title = "MN COVID-19 testing", subtitle = "Date since Mar20", x = "")+
-  theme_minimal()   
+  theme_minimal()+
+  theme(plot.subtitle = element_text(size = 8))
 
-  ggsave(filename = "covidTestGrowthRate.png", plot = gridExtra::grid.arrange(p2, p1, nrow = 1, ncol = 2), device = "png", width = 1.5, height = 1, scale = 4.5, units = "in")
+
+  ggsave(filename = "covidTestGrowthRate.pdf", plot = gridExtra::grid.arrange(p2, p1, nrow = 1, ncol = 2), device = "pdf", width = 1.5, height = 1, scale = 4.5, units = "in")
   
 
   
